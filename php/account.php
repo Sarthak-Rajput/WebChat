4baff755ec1784f36842ae844cce2cdfedcade0d
Sarthak-Rajput <75081775+Sarthak-Rajput@users.noreply.github.com>
2023-08-12 17:13:33 +0530

    account.php

--- php/account.php
+++ php/account.php
@@ -7,7 +7,7 @@
    $sql = mysqli_query($connect,"SELECT * FROM users WHERE NOT unique_id = {$outgoing_id}");
    $output = "";
    
-   if(mysqli_num_rows($sql)==1){
+   if(mysqli_num_rows($sql)==0){
         $output .= "No one is online ";
    }elseif(mysqli_num_rows($sql)>0){
        include 'data.php';
